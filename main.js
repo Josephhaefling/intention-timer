5b873d0d6de96337dfa2f51b484206f2009d38c1
Joe Haefling <josephhaefling@gmail.com>
2020-02-05 16:54:51 -0700

    JavaScript study button

--- main.js
+++ main.js
@@ -1,30 +1,31 @@
 var studyBtn = document.querySelector('#study-button');
-var studyIcon = document.querySelector('#study-icon');
+var studyImage = document.querySelector('#study-image');
 var meditateBtn = document.querySelector('#meditate-button');
-var meditateIcon = document.querySelector('#meditate-icon');
+var meditateImage = document.querySelector('#meditate-image');
 var exerciseBtn = document.querySelector('#exercise-button');
-var exerciseIcon = document.querySelector('#exercise-icon');
+var exerciseImage = document.querySelector('#exercise-image');
 
 studyBtn.addEventListener('click', studyClicked);
 meditateBtn.addEventListener('click', meditateClicked);
 exerciseBtn.addEventListener('click', exerciseClicked);
 
 function studyClicked() {
-  studyIcon.src = "assets/study-active.svg";
-  studyBtn.style.border = "1px #b2fd78 solid";
-  meditateIcon.src = "assets/meditate.svg";
-  exerciseIcon.src = "assets/exercise.svg";
+  studyImage.src = "assets/study-active.svg";
+  studyBtn.classList.add('study-selected');
+  meditateImage.src = "assets/meditate.svg";
+  exerciseImage.src = "assets/exercise.svg";
 }
 
 function meditateClicked() {
-  meditateIcon.src = "assets/study-active.svg";
-  meditateBtn.style.border = "1px #b2fd78 solid";
-  studyIcon.src = "assets/study.svg";
-  exerciseIcon.src = "assets/exercise.svg";
+  meditateImage.src = "assets/meditate-active.svg";
+  meditateBtn.classList.add("meditate-selected");
+  studyImage.src = "assets/study.svg";
+  exerciseImage.src = "assets/exercise.svg";
 }
 
 function exerciseClicked() {
-  exerciseIcon.src = "assets/study-active.svg";
-  exerciseBtn.style.border = "1px #b2fd78 solid";
-  meditateIcon.src = "assets/meditate.svg";
-  studyIcon.src = "assets/study.svg";
+  exerciseImage.src = "assets/exercise-active.svg";
+  studyBtn.classList.add('exercise-selected');
+  meditateImage.src = "assets/meditate.svg";
+  studyImage.src = "assets/exercise.svg";
+}
