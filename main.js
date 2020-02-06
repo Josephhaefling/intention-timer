9dde1e9d1e8eaf05c75baabe497cefcd15eb9bef
Melissa VanKempen <melissa.vankempen@gmail.com>
2020-02-05 18:55:32 -0700

    min-sec-buttons

--- main.js
+++ main.js
@@ -4,10 +4,14 @@ var meditateBtn = document.querySelector('#meditate-button');
 var meditateImage = document.querySelector('#meditate-image');
 var exerciseBtn = document.querySelector('#exercise-button');
 var exerciseImage = document.querySelector('#exercise-image');
+var minInput = document.querySelector('.min-input');
+var secInput = document.querySelector('.sec-input');
 
 studyBtn.addEventListener('click', studyClicked);
 meditateBtn.addEventListener('click', meditateClicked);
 exerciseBtn.addEventListener('click', exerciseClicked);
+minInput.addEventListener('input', numOnly);
+secInput.addEventListener('input', secOnly);
 
 function studyClicked() {
   studyImage.src = "assets/study-active.svg";
@@ -33,6 +37,18 @@ function exerciseClicked() {
   exerciseBtn.classList.add('exercise-selected');
   meditateImage.src = "assets/meditate.svg";
   meditateBtn.classList.remove ('meditate-selected');
-  studyImage.src = "assets/study.svg"; 
+  studyImage.src = "assets/study.svg";
   studyBtn.classList.remove ('study-selected');
 }
+
+function numOnly() {
+  if (minInput.value === "") {
+    minInput.value = "";
+  };
+};
+
+function secOnly() {
+  if (secInput.value === "") {
+    secInput.value = "";
+  };
+};
