d5a3ec6d90d821376579b468c908d4931ee1dd8e
Joe Haefling <josephhaefling@gmail.com>
2020-02-05 17:18:06 -0700

    Add study and meditate functionality

--- main.js
+++ main.js
@@ -13,19 +13,24 @@ function studyClicked() {
   studyImage.src = "assets/study-active.svg";
   studyBtn.classList.add('study-selected');
   meditateImage.src = "assets/meditate.svg";
+  meditateBtn.classList.remove('meditate-selected');
   exerciseImage.src = "assets/exercise.svg";
+  exerciseBtn.classList.remove('exercise-selected');
+
 }
 
 function meditateClicked() {
   meditateImage.src = "assets/meditate-active.svg";
   meditateBtn.classList.add("meditate-selected");
   studyImage.src = "assets/study.svg";
+  studyBtn.classList.remove('study-selected');
   exerciseImage.src = "assets/exercise.svg";
+  exerciseBtn.classList.remove('exercise-selected');
 }
 
 function exerciseClicked() {
   exerciseImage.src = "assets/exercise-active.svg";
   studyBtn.classList.add('exercise-selected');
   meditateImage.src = "assets/meditate.svg";
-  studyImage.src = "assets/exercise.svg";
+  studyImage.src = "assets/study.svg";
 }
